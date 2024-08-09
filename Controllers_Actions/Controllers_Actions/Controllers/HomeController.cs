2d26568e72977ecb4456f7702bf275e8f867a50f
DevarshTri <devarshtrivedi61@gmail.com>
2024-08-09 17:29:58 +0530

    1

---
+++ Controllers_Actions/Controllers_Actions/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Controllers_Actions.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        //public string Display()
+        //{
+        //    return "Devarsh Trivedi";
+        //}
+        //public int DisplayId(int id)
+        //{
+        //    return id;
+        //}
+
+        ////Content Result
+        //public ContentResult ContentResult()
+        //{
+        //    return Content("Content Result");
+        //}
+
+        ////Json Result
+        //public JsonResult JsonResult()
+        //{
+        //    var name = "Devarsh Trivedi";
+        //    //return Json(new {data = name });
+        //    return Json(name);
+        //}
+
+        ////Partial View Result
+        //public PartialViewResult PartialViewResult()
+        //{
+        //    return PartialViewResult("_PartialView");
+        //}
+    }
+}
