c40e9c39c2c01da788362c845ad3369551d41779
DevarshTri <devarshtrivedi61@gmail.com>
2024-08-10 13:30:23 +0530

    #

--- Controllers_Actions/Controllers_Actions/Controllers/HomeController.cs
+++ Controllers_Actions/Controllers_Actions/Controllers/HomeController.cs
@@ -6,20 +6,13 @@ namespace Controllers_Actions.Controllers
     {
         public IActionResult Index()
         {
-            ViewData["Data"] = "Devarsh";
-            ViewData["Data1"] = 23;
-            ViewData["Data2"] = DateTime.Now.ToLongDateString();
-
-            string[] arr = { "A", "B", "C" };
-            ViewData["Data3"] = arr;
-            ViewData["Data4"] = new List<string>()
-            {
-                "A","B","C"
-            };
+            TempData["Name"] = "Devarsh";
+            TempData.Keep();
             return View();
         }
         public IActionResult About()
         {
+            TempData.Keep();
             return View();
         }
         public IActionResult Contact()
@@ -56,3 +49,28 @@ namespace Controllers_Actions.Controllers
         //}
     }
 }
+//View Bag
+//ViewBag.Data1 = "Home";
+//ViewBag.Data2 = 24;
+//ViewBag.Data3 = DateTime.Now.ToShortDateString();
+
+//string[] arr = { "A", "B", "C" };
+//ViewBag.Data4 = arr;
+
+//ViewBag.Data5 = new List<string>()
+//{
+//    "A", "B", "C"
+//};
+//ViewData["Name"] = "Name";
+//ViewBag.Data6 = "Hii";
+//View Data
+//ViewData["Data"] = "Devarsh";
+//ViewData["Data1"] = 23;
+//ViewData["Data2"] = DateTime.Now.ToLongDateString();
+
+//string[] arr = { "A", "B", "C" };
+//ViewData["Data3"] = arr;
+//ViewData["Data4"] = new List<string>()
+//{
+//    "A","B","C"
+//};
