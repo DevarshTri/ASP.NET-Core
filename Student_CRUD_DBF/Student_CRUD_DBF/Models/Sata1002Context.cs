4b639bae3f45ba3025692535ee128037ebdd3712
DevarshTri <devarshtrivedi61@gmail.com>
2024-08-12 16:23:37 +0530

    #

---
+++ Student_CRUD_DBF/Student_CRUD_DBF/Models/Sata1002Context.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Student_CRUD_DBF.Models;
+
+public partial class Sata1002Context : DbContext
+{
+    public Sata1002Context()
+    {
+    }
+
+    public Sata1002Context(DbContextOptions<Sata1002Context> options)
+        : base(options)
+    {
+    }
+
+  
+
+    public virtual DbSet<Student> Students { get; set; }
+
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+        => optionsBuilder.UseNpgsql("connection");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+       
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.Property(e => e.Standart).HasDefaultValue(0);
+        });
+
+        
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
