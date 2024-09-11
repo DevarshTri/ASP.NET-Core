b72d8322758a667969b340bf467fc1c29b323830
DevarshTri <devarshtrivedi61@gmail.com>
2024-09-11 13:45:13 +0530

    Final Done with data table

--- STORE_DataTransfer/Models/DataTransfer.cs
+++ STORE_DataTransfer/Models/DataTransfer.cs
@@ -1,7 +1,9 @@
 ﻿using Npgsql;
 using System;
+using System.Data;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
 
 namespace STORE_DataTransfer.Models
 {
@@ -18,7 +20,7 @@ namespace STORE_DataTransfer.Models
 
         public async Task TransferDataAsync()
         {
-            using (var sourceConnection = new NpgsqlConnection(_source))
+            using (var sourceConnection = new SqlConnection(_source))
             using (var destinationConnection = new NpgsqlConnection(_destination))
             {
                 await sourceConnection.OpenAsync();
@@ -32,29 +34,33 @@ namespace STORE_DataTransfer.Models
                                   "WHERE S.EZ_STORE_ID IS NOT NULL AND S.EZ_STORE_ID > 0 AND F.ISPREDEFINED = 1 " +
                                   "ORDER BY S.EZ_STORE_ID, S.LOCATION;";
 
-                var command = new NpgsqlCommand(sourceQuery, sourceConnection);
-                var reader = await command.ExecuteReaderAsync();
+                // Load data from source database into a DataTable
+                var dataTable = new DataTable();
 
-                while (await reader.ReadAsync())
+                using (var command = new SqlCommand(sourceQuery, sourceConnection))
+                using (var adapter = new SqlDataAdapter(command))
                 {
-                    var storeId = reader["EZ_STORE_ID"];
-                    var mainUpc = Convert.ToString(reader["MAINUPC"]); // MAINUPC as string
-                    var quantity = Convert.ToDecimal(reader["QUANTITY"]);
+                    adapter.Fill(dataTable);
+                }
 
-                    // Retrieve SKU from ITM_VENDORSKU based on MAINUPC
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    var storeId = row["EZ_STORE_ID"];
+                    var mainUpc = Convert.ToString(row["MAINUPC"]);
+                    var quantity = Convert.ToDecimal(row["QUANTITY"]);
+
+                    // Create a new command and execute it to get SKU
                     var skuQuery = "SELECT SKU FROM ITM_VENDORSKU WHERE VENDORSKU = @Vendorsku";
                     var skuCommand = new NpgsqlCommand(skuQuery, destinationConnection);
-                    skuCommand.Parameters.AddWithValue("@Vendorsku",mainUpc); // Assuming mainUpc is being used directly here
-
+                    skuCommand.Parameters.AddWithValue("@Vendorsku", mainUpc);
                     var skuFromVendor = await skuCommand.ExecuteScalarAsync();
 
                     if (skuFromVendor == null)
                     {
-                        // If not found in ITM_VENDORSKU, check in ITM_ITEMUPC table
-                        skuQuery = "SELECT SKU FROM ITM_ITEMUPC WHERE MAINUPC = @MainUpc";
+                        // Check in ITM_ITEMUPC table if not found in ITM_VENDORSKU
+                        skuQuery = "SELECT SKU FROM ITM_ITEMUPC WHERE UPC = @UPC";
                         skuCommand = new NpgsqlCommand(skuQuery, destinationConnection);
-                        skuCommand.Parameters.AddWithValue("@MainUpc", mainUpc);
-
+                        skuCommand.Parameters.AddWithValue("@UPC", mainUpc);
                         skuFromVendor = await skuCommand.ExecuteScalarAsync();
                     }
 
@@ -64,42 +70,38 @@ namespace STORE_DataTransfer.Models
 
                         // Check if the store exists in the MST_STORE table
                         var storeCheckQuery = "SELECT STOREID FROM MST_STORE WHERE EZ_STORE_ID = @StoreId";
-                        var storeCheckCommand = new NpgsqlCommand(storeCheckQuery, destinationConnection);
+                        var storeCheckCommand = new SqlCommand(storeCheckQuery, sourceConnection);
                         storeCheckCommand.Parameters.AddWithValue("@StoreId", storeId);
-
                         var storeExists = await storeCheckCommand.ExecuteScalarAsync();
 
                         if (storeExists != null)
                         {
                             // Check if the SKU exists in ITM_POSSTOCK
-                            var posstQuery = "SELECT POSST_ID FROM ITM_POSSTOCK WHERE STOREID = @StoreId AND SKU = @Sku";
+                            var posstQuery = "SELECT POSST_ID FROM ITM_POSSTOCK WHERE STORE_ID = @StoreId AND SKU = @Sku";
                             var posstCommand = new NpgsqlCommand(posstQuery, destinationConnection);
                             posstCommand.Parameters.AddWithValue("@StoreId", storeId);
                             posstCommand.Parameters.AddWithValue("@Sku", sku);
-
                             var posstId = await posstCommand.ExecuteScalarAsync();
 
                             if (posstId != null && (int)posstId > 0)
                             {
-                                // Record exists, update POS stock
-                                var updateQuery = "UPDATE ITM_POSSTOCK SET POS_STOCK = @Pos_Stock WHERE STOREID = @StoreId AND SKU = @Sku";
+                                // Update POS stock
+                                var updateQuery = "UPDATE ITM_POSSTOCK SET POS_STOCK = @Pos_Stock WHERE STORE_ID = @StoreId AND SKU = @Sku";
                                 var updateCommand = new NpgsqlCommand(updateQuery, destinationConnection);
                                 updateCommand.Parameters.AddWithValue("@Pos_Stock", quantity);
                                 updateCommand.Parameters.AddWithValue("@StoreId", storeId);
                                 updateCommand.Parameters.AddWithValue("@Sku", sku);
-
                                 await updateCommand.ExecuteNonQueryAsync();
                                 Console.WriteLine($"Updated POS stock for SKU {sku} in store {storeId}.");
                             }
                             else
                             {
-                                // Record does not exist, insert a new record
-                                var insertQuery = "INSERT INTO ITM_POSSTOCK(STOREID, SKU, POS_STOCK) VALUES(@StoreId, @Sku, @Pos_Stock)";
+                                // Insert new record
+                                var insertQuery = "INSERT INTO ITM_POSSTOCK(STORE_ID, SKU, POS_STOCK) VALUES(@StoreId, @Sku, @Pos_Stock)";
                                 var insertCommand = new NpgsqlCommand(insertQuery, destinationConnection);
                                 insertCommand.Parameters.AddWithValue("@StoreId", storeId);
                                 insertCommand.Parameters.AddWithValue("@Sku", sku);
                                 insertCommand.Parameters.AddWithValue("@Pos_Stock", quantity);
-
                                 await insertCommand.ExecuteNonQueryAsync();
                                 Console.WriteLine($"Inserted new POS stock record for SKU {sku} in store {storeId}.");
                             }
